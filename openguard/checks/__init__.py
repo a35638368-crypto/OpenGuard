788ec448a6e0718874b1ab48108613f39a977390
Kaio Labs <a35638368@gmail.com>
2026-09-07 17:28:25 +0800

    Connect security check modules

--- openguard/checks/__init__.py
+++ openguard/checks/__init__.py
@@ -1,1 +1,7 @@
 """Security check modules for OpenGuard."""
+
+from .ssh import check_ssh
+from .ports import check_ports
+from .firewall import check_firewall
+
+__all__ = ["check_ssh", "check_ports", "check_firewall"]
